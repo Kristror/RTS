a00ee890a67dc5cc624048a46ebcc55163f4316a
Kristror <daniil.bezy@ya.ru>
2021-10-03 12:18:16 +0500

    Homework1
    Added buttons for right bottom in UI;
    SelectedObjects have outline;

---
+++ Assets/_Root/Scripts/Core/MainBuilding.cs
@@ -0,0 +1,37 @@
+using Abstractions;
+using UnityEngine;
+
+namespace Core
+{
+    public sealed class MainBuilding : MonoBehaviour, IUnitProducer, ISelectable
+    {
+        public float Health => _health;
+        public float MaxHealth => _maxHealth;
+        public Sprite Icon => _icon; 
+        public Outline OutlineModule => _outline;
+
+        [SerializeField] private GameObject _unitPrefab;
+        [SerializeField] private Transform _unitsParent;
+
+        [SerializeField] private float _maxHealth = 1000;
+        [SerializeField] private Sprite _icon;
+
+        private Outline _outline;
+
+        private float _health = 1000;
+
+        public void Awake()
+        {
+            _outline = gameObject.GetComponent<Outline>();
+            _outline.enabled = false;
+        }
+
+        public void ProduceUnit()
+        {
+            Instantiate(_unitPrefab,
+                new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10)),
+                Quaternion.identity,
+                _unitsParent);
+        }
+    }
+}
---
+++ Assets/_Root/Scripts/UserControlSystem/UI/Presenter/OutlinePresenter.cs
@@ -0,0 +1,24 @@
+using Abstractions;
+using UnityEngine;
+
+namespace UserControlSystem
+{
+    public class OutlinePresenter : MonoBehaviour
+    {
+        [SerializeField] private SelectableValue _selectedValue;
+
+        private ISelectable _currentValue;
+
+        private void Start()
+        {
+            _selectedValue.OnSelected += ONSelected;
+            ONSelected(_selectedValue.CurrentValue);
+        }
+        private void ONSelected(ISelectable selected)
+        {
+            if (_currentValue != null) _currentValue.OutlineModule.enabled = false;
+            if (selected != null) selected.OutlineModule.enabled = true;
+            _currentValue = selected;
+        }
+    }
+}
